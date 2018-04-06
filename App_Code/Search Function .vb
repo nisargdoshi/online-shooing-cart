20ca8a5dd6024eff859da1bf744297ea5042ab3c
Nisarg Doshi <15bit157@nirmauni.ac.in>
2018-04-06 15:47:19 +0530

    Add files via upload

---
+++ App_Code/Search Function .vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Search_Function_By_Harsh
+    'If radClient.Checked = True Then
+    Dim search As String
+    Dim query As String
+    Dim data_source As SqlCacheDependency
+End Class
